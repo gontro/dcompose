fe03e39c71778b700cb11492b7a863d41efe1629
Krystan Gontscharow <gontro@gmail.com>
2016-02-18 22:03:59 +1100

    Use custom dcompose.yml files for clarity.

--- dcompose.sh
+++ dcompose.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+CONFIG_FILE="dcompose.yml"
+CONFIG_PATH="`pwd`/${CONFIG_FILE}"
+
 BOOTSTRAP_FILE="bootstrap.conf"
 BOOTSTRAP_PATH="`pwd`/${BOOTSTRAP_FILE}"
 
 # Must pass at least one argument to docker-compose
 if [[ ! $1 ]]; then
     echo "Usage: dcompose <docker_compose_arg1> [<docker_compose_arg2>, ...]" 1>&2
+    exit 1
+fi
+
+# Ensure dcompose.yml exists
+if [[ ! -a "${CONFIG_PATH}" ]]; then
+    echo "Error: cannot find '${CONFIG_FILE}'" 1>&2
     exit 1
 fi
 
@@ -28,4 +37,4 @@ else
 fi
 
 # Run docker-compose commands
-docker-compose "$@"
+docker-compose -f=${CONFIG_PATH} "$@"
